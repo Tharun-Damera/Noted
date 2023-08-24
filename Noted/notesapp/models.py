0c0a0093d76653118a267a37d8d1a7b30fb571b0
Tharun Damera <89800924+Tharun-Damera@users.noreply.github.com>
2023-08-24 20:18:23 +0530

    Code Upload

---
+++ Noted/notesapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Notes(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.title
+
+
+class Trash(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.title
+    
